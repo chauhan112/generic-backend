a03472bfd9e166b38a3b86f7f8f3891ae308cefe
Raja <rajababuchauhan500@gmail.com>
2026-01-12 16:30:57 +0100

    simple handler example

---
+++ handlers/simpleHandler.py
@@ -0,0 +1,9 @@
+from handlers.interface import IHandler
+
+class SimpleHandler(IHandler):
+    def is_current_path(self, path: str) -> bool:
+        return path == "/test"
+    def validate_params(self, params: dict):
+        return True
+    def call_handler(self, params: dict):
+        return {"message": "Hello, World!"}
